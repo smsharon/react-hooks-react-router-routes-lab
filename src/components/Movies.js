297b469cbca4abce153d78b8910f713595f3aef2
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-08-31 14:02:55 -0400

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    {/*{code here}*/}
+    <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h2>{movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
+    </div>
+    );
 }
 
 export default Movies;
